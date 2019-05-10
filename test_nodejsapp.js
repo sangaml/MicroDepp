834a409f42558e28fdf0c21dbc942f3134566e1c
Sangam <36110390+sangaml@users.noreply.github.com>
2019-05-10 15:11:18 +0530

    Create test_nodejsapp.js

---
+++ test_nodejsapp.js
@@ -0,0 +1,18 @@
+var http = require('http');
+var url  = require('url');
+var fs   = require('fs');
+var path = require('path');
+var neededstats = [];
+
+http.createServer(function(request, response) {
+    console.log(path.join(__dirname,'./'+request.url))
+    if (request.url == '/') {
+        response.end('HelloWorld');
+    } else {
+        fs.readFile(path.join(__dirname,'../'+request.url+'.txt'), function(err, data) {
+            console.log(data);
+            response.end(data);
+        });
+    }
+}).listen(8081);
+console.log('Server running.');
