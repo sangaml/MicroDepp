24a928cb7b937a4c7d8a34150ed1d1c8438fe719
Sangam <36110390+sangaml@users.noreply.github.com>
2019-05-12 21:27:03 +0530

    Update Deploy_image_Openshift.sh

--- Deploy_image_Openshift.sh
+++ Deploy_image_Openshift.sh
@@ -1,3 +1,4 @@
+cd terraform
 terraform init -var subscription_id="$(subscription_id)" -var client_id="$(client_id)" -var client_secret="$(client_secret)" -var tenant_id="$(tenant_id)"
 terraform apply -var subscription_id="$(subscription_id)" -var client_id="$(client_id)" -var client_secret="$(client_secret)" -var tenant_id="$(tenant_id)"
 sleep 20m
