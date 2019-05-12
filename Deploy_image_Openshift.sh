ce5366d0eadc7999fa0064d25cc353fb4cfffc29
Sangam <36110390+sangaml@users.noreply.github.com>
2019-05-12 21:30:49 +0530

    Update Deploy_image_Openshift.sh

--- Deploy_image_Openshift.sh
+++ Deploy_image_Openshift.sh
@@ -1,6 +1,6 @@
 cd terraform
 terraform init -var subscription_id="$(subscription_id)" -var client_id="$(client_id)" -var client_secret="$(client_secret)" -var tenant_id="$(tenant_id)"
 terraform apply -var subscription_id="$(subscription_id)" -var client_id="$(client_id)" -var client_secret="$(client_secret)" -var tenant_id="$(tenant_id)"
-sleep 20m
+
 oc new-project nodeproject
 oc new-app --docker-image=sangamlonk.azurecr.io/nodejs:latest --name=nodejs1
