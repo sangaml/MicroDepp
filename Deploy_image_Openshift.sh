47b11ab4ac663ade63ac20c8dd28233a8013d5ae
Sangam <36110390+sangaml@users.noreply.github.com>
2019-05-13 07:33:01 +0530

    Update Deploy_image_Openshift.sh

--- Deploy_image_Openshift.sh
+++ Deploy_image_Openshift.sh
@@ -1,4 +1,4 @@
-cd terraform
+cd /home/admin123/myagent/tppo_workdir/1/a/terraform/
 terraform init -var subscription_id="$(subscription_id)" -var client_id="$(client_id)" -var client_secret="$(client_secret)" -var tenant_id="$(tenant_id)"
 terraform apply -var subscription_id="$(subscription_id)" -var client_id="$(client_id)" -var client_secret="$(client_secret)" -var tenant_id="$(tenant_id)"
 
