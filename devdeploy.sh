52312cc7f582fa92061a334fd1296a977da6d57f
rajeev-world <rajeev.gupta@globant.com>
2019-05-14 12:16:32 +0530

    Update devdeploy.sh

--- devdeploy.sh
+++ devdeploy.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-	 oc login https://api.pro-us-east-1.openshift.com --token=$ostoken
-	 oc project nodejsapp
-	 oc tag sangamlonk.azurecr.io/nodejsms:BUILD_BUILDNUMBER  nodejsapp/nodejsms:latest
+echo $ostoken
+echo $(ostoken)
+oc login https://api.pro-us-east-1.openshift.com --token=$(ostoken)
+oc project nodejsapp
+oc tag sangamlonk.azurecr.io/nodejsms:BUILD_BUILDNUMBER  nodejsapp/nodejsms:latest
