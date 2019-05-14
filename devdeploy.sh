3b5ef137cb00d40b9679de0cf29202cee800a802
rajeev-world <rajeev.gupta@globant.com>
2019-05-14 11:50:49 +0530

    Create devdeploy.sh

---
+++ devdeploy.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+	 oc login https://api.pro-us-east-1.openshift.com --token=$ostoken
+	 oc project nodejsapp
+	 oc tag sangamlonk.azurecr.io/nodejsms:BUILD_BUILDNUMBER  nodejsapp/nodejsms:latest
