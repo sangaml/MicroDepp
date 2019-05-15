236eb83a100b8c21f5899a01f61d9da84695d9fa
rajeev-world <rajeev.gupta@globant.com>
2019-05-15 18:50:02 +0530

    Delete Deploy_image_Openshift.sh

--- Deploy_image_Openshift.sh
+++
@@ -1,6 +0,0 @@
-cd /home/admin123/myagent/tppo_workdir/1/a/terraform/
-terraform init -var subscription_id="$(subscription_id)" -var client_id="$(client_id)" -var client_secret="$(client_secret)" -var tenant_id="$(tenant_id)"
-terraform apply -var subscription_id="$(subscription_id)" -var client_id="$(client_id)" -var client_secret="$(client_secret)" -var tenant_id="$(tenant_id)"
-
-oc new-project nodeproject
-oc new-app --docker-image=sangamlonk.azurecr.io/nodejs:latest --name=nodejs1
