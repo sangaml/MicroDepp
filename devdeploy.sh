c01d43adaa8113fe789a2bd637a8208bf5c52759
sangaml <sangamlonk2gmail.com>
2019-05-22 10:04:19 +0000

    added files

--- devdeploy.sh
+++
@@ -1,5 +0,0 @@
-echo $ostoken
-echo $(ostoken)
-oc login https://api.pro-us-east-1.openshift.com --token=$(ostoken)
-oc project nodejsapp
-oc tag sangamlonk.azurecr.io/nodejsms:BUILD_BUILDNUMBER  nodejsapp/nodejsms:latest
