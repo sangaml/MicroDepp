3cb28d655ce808a7d47954e60a2656579e5d204e
rajeev-world <rajeev.gupta@globant.com>
2019-06-03 15:32:35 +0530

    Update dev_stage.sh

--- dev_stage.sh
+++ dev_stage.sh
@@ -6,5 +6,6 @@ projectname=microdepp
 cat _sangaml.MicroDepp/drop/version.txt
 oc login $osurl --token=$ostoken	
 # oc project $projectname
+
 oc tag $dockerId.azurecr.io/$dbimageName:$BUILD_BUILDNUMBER  $projectname/mongodb:latest 
 oc tag $dockerId.azurecr.io/$appimageName:$BUILD_BUILDNUMBER  $projectname/rsvp:latest
