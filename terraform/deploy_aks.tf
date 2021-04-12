9190d841eabfd56d9db51d4027aefddac2672ff5
sangaml <sangamlonk@gmail.com>
2021-04-12 12:31:58 +0530

    uncomment few lines

--- terraform/deploy_aks.tf
+++ terraform/deploy_aks.tf
@@ -1,6 +1,6 @@
 provider "azurerm" {
   # Whilst version is optional, we /strongly recommend/ using it to pin the version of the Provider being used
-  #version = "=1.27.0"
+  version = "=2.51.0"
   subscription_id = "${var.subscription_id}"
   client_id       = "${var.client_id}"
   client_secret   = "${var.client_secret}"
