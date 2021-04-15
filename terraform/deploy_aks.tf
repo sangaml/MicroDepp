79ded4b31d0f2d0abf400449bad0399c43eb3c03
sangaml <sangamlonk@gmail.com>
2021-04-15 13:02:13 +0530

    added env variable

--- terraform/deploy_aks.tf
+++ terraform/deploy_aks.tf
@@ -43,10 +43,10 @@ provider "azurerm" {
   # Whilst version is optional, we /strongly recommend/ using it to pin the version of the Provider being used
   #version = "=1.37.0"
   features {}
-  subscription_id = var.subscription_id
-  client_id       = var.client_id
-  client_secret   = var.client_secret
-  tenant_id       = var.tenant_id
+#  subscription_id = var.subscription_id
+#  client_id       = var.client_id
+#  client_secret   = var.client_secret
+#  tenant_id       = var.tenant_id
 }
 
 resource "azurerm_resource_group" "test" {
